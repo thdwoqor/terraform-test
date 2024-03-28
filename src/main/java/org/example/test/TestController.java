c3ad552966f2bf7b0663b84ca65a6ae4e39f3d81
thdwoqor <celebarte01@gmail.com>
2024-03-28 17:47:34 +0900

    feat: 컨트롤러 패키지 수정

--- src/main/java/org/example/test/TestController.java
+++ src/main/java/org/example/test/TestController.java
@@ -1,10 +1,12 @@
+package org.example.test;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
 
-    @GetMapping("test")
+    @GetMapping("/test")
     public String test() {
         return "test";
     }
